692835e81fd96e4267b9479856fa01e20c693402
Michael Hammer <mhammer708@gmail.com>
2020-05-07 16:27:52 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Spotify from "spotify-web-api-js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const spotifyWebApi = new Spotify();
+
+class App extends React.Component {
+  constructor() {
+    super();
+    const params = this.getHashParams();
+    this.state = {
+      loggedIn: params.access_token ? true : false,
+      nowPlaying: { name: "Not Checked", image: "" },
+    };
+    if (params.access_token) {
+      spotifyWebApi.setAccessToken(params.access_token);
+    }
+  }
+
+  getHashParams() {
+    var hashParams = {};
+    var e,
+      r = /([^&;=]+)=?([^&;]*)/g,
+      //adding the access token to URL
+      q = window.location.hash.substring(1);
+    while ((e = r.exec(q))) {
+      //updating URL bar
+      hashParams[e[1]] = decodeURIComponent(e[2]);
+    }
+    return hashParams;
+
+    //instead, make a call to write it to firebase
+  }
+
+  getNowPlaying() {
+    console.log("clicked");
+    spotifyWebApi.getMyCurrentPlaybackState().then((response) => {
+      this.setState({
+        nowPlaying: {
+          name: response.item.name,
+          image: response.item.album.images[0].url,
+        },
+      });
+      console.log("response", response);
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <div>Now Playing: {this.state.nowPlaying.name}</div>
+          <img src={this.state.nowPlaying.image} style={{ width: 100 }} />
+          <a href="http://localhost:8888">
+            <button>Login with Spotify</button>
+          </a>
+          <a>
+            <button onClick={() => this.getNowPlaying()}>
+              Get Now Playing
+            </button>
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
