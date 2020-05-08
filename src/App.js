fea3e13e0f03c1ab16e2958b52c348f1828e98b9
Alona Trekhlib <trekhleb.al@gmail.com>
2020-05-07 23:12:00 -0500

    able to get refresh tocken

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import logo from "./logo.svg"
 import "./App.css"
 import { createNewRoom, getRoom, getCurrentRoomData } from "./firebase/firebase"
-import SpotifyLogin from "react-spotify-login"
+//import SpotifyLogin from "react-spotify-login"
+import axios from "axios"
+import queryString from "querystring"
 
-const clientId = ""
 const redirectUri = "http://localhost:3000"
+const clientId = "5831023fb4004d61a610f092f1e612b4"
+const clientSecret = "a0174569adec4c988e845ace473ae66a"
+const scopes = ["user-read-currently-playing", "user-read-playback-state"]
 
 function App() {
+  const [token, setToken] = useState("")
+  const [episodeUrl, setEpisodeUrl] = useState()
+
+  useEffect(() => {
+    const code = new URLSearchParams(window.location.search).get("code")
+    if (code !== null) {
+      const accessForm = queryString.stringify({
+        grant_type: "authorization_code",
+        code,
+        redirect_uri: redirectUri,
+      })
+      // base64 encode auth data
+      const auth = btoa(`${clientId}:${clientSecret}`)
+      axios
+        .post("https://accounts.spotify.com/api/token", accessForm, {
+          headers: {
+            "content-type": "application/x-www-form-urlencoded;charset=utf-8",
+            Authorization: `Basic ${auth}`,
+          },
+        })
+        .then((res) => {
+          console.log(res)
+          // removes 'code' query param to clean up URL
+          window.history.replaceState(null, null, window.location.pathname)
+        })
+        .catch((err) => console.log(err))
+    }
+  })
+
   const buttonClick = () => {
     createNewRoom({
       name: "room11",
       password: "123",
       users: [
-        { name: "Alona", accessTocken: "someTocken", email: "some email" },
-        { name: "Justin", accessTocken: "tocken", email: "email" },
+        { name: "Alona", accessTocken: token, email: "some email" },
+        { name: "Justin", accessTocken: "token", email: "email" },
       ],
-      currentPodcast: { apiData: "somedata" },
+      currentPodcast: { apiData: episodeUrl },
     })
     getRoom("room11", "123").then((res) => getCurrentRoomData(res))
+    //getSampleData(token, "1oLdBqEIgphJN3O6ULyw4T")
   }
 
   return (
@@ -29,12 +63,19 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <SpotifyLogin
-          clientId={clientId}
-          redirectUri={redirectUri}
-          onSuccess={(response) => console.log(response)}
-          onFailure={(response) => console.log(response)}
-        />
+        <a
+          href={
+            "https://accounts.spotify.com/authorize?" +
+            queryString.stringify({
+              response_type: "code",
+              client_id: clientId,
+              scope: scopes,
+              redirect_uri: redirectUri,
+            })
+          }
+        >
+          Login to Spotify
+        </a>
       </header>
     </div>
   )
