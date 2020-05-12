05fbee06650012a1ac1e6d6f853d620cf9ce601b
Alona Trekhlib <trekhleb.al@gmail.com>
2020-05-12 10:40:08 -0500

    login works, clean up code

--- src/App.js
+++ src/App.js
@@ -1,82 +1,31 @@
-import React, { useState, useEffect } from "react"
+import React, { useEffect } from "react"
 import "./App.css"
-import { createNewRoom, getRoom, getCurrentRoomData } from "./firebase/firebase"
-import { spotifyLogin, getNewToken, getMyData } from "./spotifyLogin"
+import { spotifyLogin } from "./spotifyLogin"
 import { getAccessToken, setSpotifyCode, getUserData } from "./redux/store"
-import axios from "axios"
-import queryString from "querystring"
 import { connect } from "react-redux"
-import { BrowserRouter, Route, Switch, NavLink } from "react-router-dom"
-
-const redirectUri = "http://localhost:3000"
-const clientId = "5831023fb4004d61a610f092f1e612b4"
-const clientSecret = "a0174569adec4c988e845ace473ae66a"
 
 function App(props) {
   console.log(props)
-  const [token, setToken] = useState("")
-  const [refreshToken, setRefreshToken] = useState()
-  const [episodeUrl, setEpisodeUrl] = useState()
-  const [mySpotifyData, setMySpotifyData] = useState()
 
   useEffect(() => {
-    if(!props.code) {
-      console.log('no props.code --> need to set')
+    if (!props.code) {
+      console.log("no props.code --> need to set")
       let code = new URLSearchParams(window.location.search).get("code")
-      if(code) {
-        console.log('SPOTIFY CODE FROM URL', code)
+      if (code) {
+        console.log("SPOTIFY CODE FROM URL", code)
         props.setSpotifyCode(code)
         props.getAccessToken(code)
       }
     }
-    console.log('inside useEffect', props)
+    console.log("inside useEffect", props)
   }, [])
-
-  // const getSampleData = async (token, episodeId) => {
-  //   try {
-  //     const episode = await axios.get(
-  //       `https://api.spotify.com/v1/episodes/${episodeId}`,
-  //       {
-  //         headers: {
-  //           Authorization: "Bearer " + token,
-  //         },
-  //       }
-  //     )
-  //     console.log("EPISODE", episode)
-  //     setEpisodeUrl(episode)
-  //   } catch (err) {
-  //     console.log(err)
-  //   }
-  // }
-  const buttonClick = () => {
-    if (mySpotifyData && token) {
-      console.log("sending data")
-      createNewRoom({
-        name: "room11",
-        password: "123",
-        users: [
-          {
-            name: mySpotifyData.display_name,
-            accessToken: token,
-            refreshToken: refreshToken,
-          },
-          { name: "Justin", accessToken: "token" },
-        ],
-        currentPodcast: { apiData: "" },
-      })
-    }
-
-    getRoom("room11", "123").then((res) => getCurrentRoomData(res))
-    //getSampleData(token, "1oLdBqEIgphJN3O6ULyw4T")
-    props.getUserData(props.access_token)
-  }
 
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={buttonClick}>Button</button>
-        {mySpotifyData && <div>Hello, {mySpotifyData.display_name}</div>}
-        <button onClick={() => spotifyLogin(props.code)}>Login to Spotify</button>
+        <button onClick={() => spotifyLogin(props.code)}>
+          Login to Spotify
+        </button>
       </header>
     </div>
   )
