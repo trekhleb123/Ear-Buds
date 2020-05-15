1300dc4c3f7ee5808f63d504b73b08ef60fe36d8
Michael Hammer <mhammer708@gmail.com>
2020-05-15 12:55:42 -0500

    fixed messages issue

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,4 +1,4 @@
-import firebase from "firebase"
+import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
   // copy and paste your firebase credential here
@@ -8,97 +8,94 @@ const firebaseApp = firebase.initializeApp({
   projectId: "podcastparty-402e2",
   storageBucket: "gs://podcastparty-402e2.appspot.com",
   messagingSenderId: "311285409494",
-})
+});
 
-const db = firebaseApp.firestore()
+const db = firebaseApp.firestore();
 //const firestore = getFirestore()
-const firestore = firebase.firestore()
-export { db, firestore }
+const firestore = firebase.firestore();
+export { db, firestore };
 
 export async function createNewRoom(newRoom) {
   try {
-    const room = await db.collection("Rooms").add(newRoom)
-    console.log(room)
+    const room = await db.collection("Rooms").add(newRoom);
+    console.log(room);
   } catch (err) {
-    console.error(err)
+    console.error(err);
   }
 }
 
-export async function getRoom(roomName, roomPassword) {
+export async function getRoom(roomCode) {
   try {
-    const rooms = db.collection("Rooms")
-    const currentRoom = await rooms
-      .where("name", "==", roomName)
-      .where("password", "==", roomPassword)
-      .get()
-    let res = {}
+    const rooms = db.collection("Rooms");
+    const currentRoom = await rooms.where("roomCode", "==", roomCode).get();
+    let res = {};
     currentRoom.forEach((el) => {
-      res = el.id
-    })
-    console.log(res)
-    return res
+      res = el.id;
+    });
+    console.log(res);
+    return res;
   } catch (err) {
-    console.error(err)
+    console.error(err);
   }
 }
 
 export async function getCurrentRoomData(docId) {
   try {
-    const doc = db.collection("Rooms").doc(docId)
-    const result = await doc.get()
+    const doc = db.collection("Rooms").doc(docId);
+    const result = await doc.get();
 
-    console.log(result.data())
-    return result.data()
+    console.log(result.data());
+    return result.data();
   } catch (err) {
-    console.error(err)
+    console.error(err);
   }
 }
 
 export async function getCurrentUserData(docId, callback) {
   try {
-    const users = db.collection("Rooms").doc(docId).collection("Users")
-    const result = await users.get()
+    const users = db.collection("Rooms").doc(docId).collection("Users");
+    const result = await users.get();
 
-    result.forEach((user) => console.log(user.id, "=>", user.data()))
+    result.forEach((user) => console.log(user.id, "=>", user.data()));
 
     // console.log(result.data());
-    return result
+    return result;
   } catch (err) {
-    console.error(err)
+    console.error(err);
   }
 }
 export async function getRooms() {
-  const doc = db.collection('Rooms')
- const docs = await doc.get()
-     let res = []
+  const doc = db.collection("Rooms");
+  const docs = await doc.get();
+  let res = [];
   docs.forEach((el) => {
-    res.push(el)
-  })
-  console.log('res', res)
-  return res
+    res.push(el);
+  });
+  console.log("res", res);
+  return res;
 }
 
 export async function createRoom(token, username, refreshToken) {
   //event.preventDefault()
   const code =
     Math.random().toString(36).substring(2, 7) +
-    Math.random().toString(36).substring(2, 7)
-  console.log("in handle submit", code)
+    Math.random().toString(36).substring(2, 7);
+  console.log("in handle submit", code);
   const newRoom = await db
     .collection("Rooms")
-    .add({ name: "room1", roomCode: code })
-  console.log("newRoom", newRoom)
+    .add({ name: "room1", roomCode: code });
+  console.log("newRoom", newRoom);
   await db.collection("Rooms").doc(newRoom.id).collection("Users").add({
     accessToken: token,
     name: username,
     roomCode: code,
     deviceId: 2,
     refreshToken,
-  })
+  });
 
-  await db.collection("Rooms").doc(newRoom.id).collection("messages").add({})
+  await db.collection("Rooms").doc(newRoom.id).collection("messages").add({});
 
-  return newRoom.id
+  return code;
 }
 
 export async function joinRoom(token, username, refreshToken, res, roomCode) {
@@ -108,63 +105,62 @@ export async function joinRoom(token, username, refreshToken, res, roomCode) {
     roomCode: roomCode,
     deviceId: 2,
     refreshToken,
-  })
+  });
 }
 
 export async function findRoom(roomCode) {
-  const rooms = db.collection("Rooms")
-  const currentRoom = await rooms.where("roomCode", "==", roomCode).get()
-  let res = {}
+  const rooms = db.collection("Rooms");
+  const currentRoom = await rooms.where("roomCode", "==", roomCode).get();
+  let res = {};
   currentRoom.forEach((el) => {
-    res = el.id
-  })
-  console.log("currentroom", res, currentRoom)
-  return res
+    res = el.id;
+  });
+  console.log("currentroom", res, currentRoom);
+  return res;
 }
 export async function userLeft(roomId, displayName) {
-  const users = await db
-    .collection('Rooms')
-    .doc(roomId)
-    .collection('Users');
-  const currentUser = await users
-    .where('name', '==', displayName)
-    .get();
+  const users = await db.collection("Rooms").doc(roomId).collection("Users");
+  const currentUser = await users.where("name", "==", displayName).get();
   let res = {};
-  currentUser.forEach(el => {
+  currentUser.forEach((el) => {
     res = el.id;
   });
   console.log(res);
   await db
-    .collection('Rooms')
+    .collection("Rooms")
     .doc(roomId)
-    .collection('Users')
+    .collection("Users")
     .doc(res)
     .delete();
 }
 export async function renderUsers(roomId) {
-  const obj = {}
-    await db.collection("Rooms").doc(roomId).collection('Users').get()
-    .then(function(room) {
-      room.forEach(function(doc) {
+  const obj = {};
+  await db
+    .collection("Rooms")
+    .doc(roomId)
+    .collection("Users")
+    .get()
+    .then(function (room) {
+      room.forEach(function (doc) {
         //console.log(doc.id, " => ", doc.data());
-        obj[doc.id] = doc.data().name
+        obj[doc.id] = doc.data().name;
+      });
+      //console.log(obj)
     });
-    //console.log(obj)
-  });
-    return obj
+  return obj;
 }
 
 export async function vacantRoom(roomId) {
   let userLength;
-    const users = await db
-    .collection('Rooms')
+  const users = await db
+    .collection("Rooms")
     .doc(roomId)
-    .collection('Users')
+    .collection("Users")
     .get()
-    .then(function(user) {
-    userLength = user.size
-   })
-   if(userLength === 0) {
-    await db.collection("Rooms").doc(roomId).delete()
-   }
+    .then(function (user) {
+      userLength = user.size;
+    });
+  if (userLength === 0) {
+    await db.collection("Rooms").doc(roomId).delete();
+  }
 }
