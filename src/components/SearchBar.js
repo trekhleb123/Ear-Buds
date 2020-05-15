3a7b084c9908d5ef248abfa9212a05b76385f3c9
Michael Hammer <mhammer708@gmail.com>
2020-05-15 14:53:30 -0500

    pushing to master

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,36 +1,36 @@
-import React, { useState, useEffect } from "react"
-import AsyncSelect from "react-select/async"
-import axios from "axios"
-import Autocomplete from "@material-ui/lab/Autocomplete"
-import TextField from "@material-ui/core/TextField"
-import ListItem from "@material-ui/core/ListItem"
-import Select from "@material-ui/core/Select"
-import InputLabel from "@material-ui/core/InputLabel"
-import FormControl from "@material-ui/core/FormControl"
-import Player from "./Player"
-import DropDownMenu from "material-ui/DropDownMenu"
-import MenuItem from "material-ui/MenuItem"
-import Button from "@material-ui/core/Button"
-import { getAccessToken, setSpotifyCode, getUserData } from "../redux/store"
-import { connect } from "react-redux"
-import { getEpisode, fetchEpisodes, fetchShows } from "../api/spotifyApi"
-import { changeQueue } from "../firebase/firebase"
+import React, { useState, useEffect } from "react";
+import AsyncSelect from "react-select/async";
+import axios from "axios";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import TextField from "@material-ui/core/TextField";
+import ListItem from "@material-ui/core/ListItem";
+import Select from "@material-ui/core/Select";
+import InputLabel from "@material-ui/core/InputLabel";
+import FormControl from "@material-ui/core/FormControl";
+import Player from "./Player";
+import DropDownMenu from "material-ui/DropDownMenu";
+import MenuItem from "material-ui/MenuItem";
+import Button from "@material-ui/core/Button";
+import { getAccessToken, setSpotifyCode, getUserData } from "../redux/store";
+import { connect } from "react-redux";
+import { getEpisode, fetchEpisodes, fetchShows } from "../api/spotifyApi";
+import { changeQueue } from "../firebase/firebase";
 
 const SearchBar = (props) => {
-  const token = props.token
+  const token = props.token;
   // const [state, setState] = useState({
   //   epId: "",
   // });
-  let [search, setSearch] = useState("")
-  let [result, setResult] = useState([])
-  let [episodes, setEpisodes] = useState([])
-  let [chosenEpisode, setEpisode] = useState()
-  let [uri, setUri] = useState()
+  let [search, setSearch] = useState("");
+  let [result, setResult] = useState([]);
+  let [episodes, setEpisodes] = useState([]);
+  let [chosenEpisode, setEpisode] = useState();
+  let [uri, setUri] = useState();
   let [results, setResults] = useState([
     { value: "chocolate", label: "Chocolate" },
-  ])
+  ]);
   const searchHandler = async () => {
-    const res = await fetchShows(search, token, 50)
+    const res = await fetchShows(search, token, 50);
 
     // const q = encodeURIComponent(`${search}`);
     // const response = await fetch(
@@ -44,16 +44,16 @@ const SearchBar = (props) => {
     // );
     // const searchJSON = await response.json();
 
-    let searchArr = [{ value: "chocolate", label: "Chocolate" }]
+    let searchArr = [{ value: "chocolate", label: "Chocolate" }];
 
     if (res.shows) {
       searchArr = res.shows.items.map((item) => {
-        return { value: item.id, label: item.name }
-      })
+        return { value: item.id, label: item.name };
+      });
     }
-    console.log("search arr!!", searchArr)
-    setResults(searchArr)
-  }
+    console.log("search arr!!", searchArr);
+    setResults(searchArr);
+  };
 
   // useEffect(() => {
   //   const foo = async function () {
@@ -63,28 +63,28 @@ const SearchBar = (props) => {
   // }, [props.search]);
 
   const handleChange = async (event) => {
-    const name = event.target.name
-    const value = event.target.value
+    const name = event.target.name;
+    const value = event.target.value;
     // setState({
     //   ...state,
     //   [name]: value,
     // });
     getEpisode(value, token).then((res) =>
       changeQueue(props.roomId, res, value, props.userData.display_name)
-    )
-  }
+    );
+  };
 
   const activeSearch = async (text) => {
-    setSearch(text)
-    await searchHandler()
-  }
+    setSearch(text);
+    await searchHandler();
+  };
 
   const getEpisodes = async () => {
     fetchShows(search, token, 1)
       .then((res) => {
         result = res.shows.items.map((item) => {
-          return item.id
-        })
+          return item.id;
+        });
       })
       .then(() => setResult(result))
       .then(() => fetchEpisodes(result, token))
@@ -95,11 +95,11 @@ const SearchBar = (props) => {
             name: item.name,
             date: item.release_date,
             id: item.id,
-          }
-        })
+          };
+        });
       })
-      .then((res) => setEpisodes(res))
-  }
+      .then((res) => setEpisodes(res));
+  };
 
   return (
     <div>
@@ -114,7 +114,7 @@ const SearchBar = (props) => {
             {...params}
             onChange={({ target }) => {
               // activeSearch(target.value);
-              activeSearch(target.value)
+              activeSearch(target.value);
             }}
             label="Search input"
             margin="normal"
@@ -142,8 +142,8 @@ const SearchBar = (props) => {
                     key={episode.id}
                     value={episode.id}
                     onClick={() => {
-                      getEpisode(episode.id)
-                      setUri(episode.uri)
+                      getEpisode(episode.id);
+                      setUri(episode.uri);
                     }}
                   >
                     {episode.name}
@@ -175,11 +175,11 @@ const SearchBar = (props) => {
         episode={chosenEpisode}
       />
     </div>
-  )
-}
+  );
+};
 const stateToProps = (state) => ({
   token: state.access_token,
   userData: state.userData,
-})
+});
 
-export default connect(stateToProps)(SearchBar)
+export default connect(stateToProps)(SearchBar);
--- src/components/SingleRoom.js
+++ src/components/SingleRoom.js
@@ -1,36 +1,33 @@
-import React from "react"
-import Messages from "./Messages"
-import { db, userLeft, renderUsers, vacantRoom } from "../firebase/firebase"
-import { getAccessToken, setSpotifyCode, getUserData } from "../redux/store"
-import { connect } from "react-redux"
-import SearchBar from "./SearchBar"
+import React from "react";
+import Messages from "./Messages";
+import { db, userLeft, renderUsers, vacantRoom } from "../firebase/firebase";
+import { getAccessToken, setSpotifyCode, getUserData } from "../redux/store";
+import { connect } from "react-redux";
+import SearchBar from "./SearchBar";
 
 class SingleRoom extends React.Component {
   constructor() {
-    super()
+    super();
     this.state = {
       users: {},
-    }
-    this.leaveRoom = this.leaveRoom.bind(this)
+    };
+    this.leaveRoom = this.leaveRoom.bind(this);
   }
   async componentDidMount() {
     if (!Object.keys(this.props.userData).length) {
-      this.props.history.push("/")
+      this.props.history.push("/");
     }
-    this.props.getUserData(this.props.access_token)
-    await renderUsers(this.props.match.params.roomId)
+    this.props.getUserData(this.props.access_token);
+    await renderUsers(this.props.match.params.roomId);
     this.setState({
       users: await renderUsers(this.props.match.params.roomId),
-    })
+    });
   }
 
   async leaveRoom(roomId, displayName) {
-    await userLeft(
-      this.props.match.params.roomId,
-      this.props.userData.display_name
-    )
-    await vacantRoom(this.props.match.params.roomId)
-    this.props.history.push("/")
+    await userLeft(roomId, displayName);
+    await vacantRoom(roomId);
+    this.props.history.push("/");
   }
 
   render() {
@@ -39,29 +36,37 @@ class SingleRoom extends React.Component {
         <h2>Users</h2>
         <div>
           {Object.values(this.state.users).map((user) => {
-            return <li key={user}>{user}</li>
+            return <li key={user}>{user}</li>;
           })}
         </div>
-        <button type="button" onClick={this.leaveRoom}>
+        <button
+          type="button"
+          onClick={() =>
+            this.leaveRoom(
+              this.props.match.params.roomId,
+              this.props.userData.display_name
+            )
+          }
+        >
           Leave Room
         </button>
         <Messages />
         <SearchBar roomId={this.props.match.params.roomId} />
         <button type="button">Invite Friend</button>
       </div>
-    )
+    );
   }
 }
 const stateToProps = (state) => ({
   access_token: state.access_token,
   userData: state.userData,
   refresh_token: state.refresh_token,
-})
+});
 
 const dispatchToProps = (dispatch) => ({
   getAccessToken: (code) => dispatch(getAccessToken(code)),
   setSpotifyCode: (code) => dispatch(setSpotifyCode(code)),
   getUserData: (token) => dispatch(getUserData(token)),
-})
+});
 
-export default connect(stateToProps, dispatchToProps)(SingleRoom)
+export default connect(stateToProps, dispatchToProps)(SingleRoom);
