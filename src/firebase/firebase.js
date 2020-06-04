0f37fe632d373e9d280457096c3a075ae1e10931
swadhwa369 <55638179+swadhwa369@users.noreply.github.com>
2020-06-04 11:27:56 -0500

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,15 +1,6 @@
 import firebase from "firebase";
 import { getNowPlaying } from "../api/spotifyApi";
 
-const firebaseApp = firebase.initializeApp({
-  // copy and paste your firebase credential here
-  apiKey: "AIzaSyBYMe0vrHWmvoWoMrZGB62EXeEhgdNVZvM",
-  authDomain: "podcastparty-402e2.web.app",
-  databaseURL: "https://podcastparty-402e2.firebaseio.com",
-  projectId: "podcastparty-402e2",
-  storageBucket: "gs://podcastparty-402e2.appspot.com",
-  messagingSenderId: "311285409494",
-});
 
 const db = firebaseApp.firestore();
 //const firestore = getFirestore()
