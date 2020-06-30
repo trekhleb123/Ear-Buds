f69be32090492ebc9577dc8154d9238006cdeca7
Alona Trekhlib <trekhleb.al@gmail.com>
2020-06-30 17:40:43 -0500

    changed redirect

--- src/spotifyLogin.js
+++ src/spotifyLogin.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 import queryString from "querystring"
 
-const redirectUri = "http://localhost:3000"
+const redirectUri = "https://earbuds.web.app"
 const clientId = process.env.REACT_APP_CLIENT_ID
 const clientSecret = process.env.REACT_APP_CLIENT_SECRET
 const scopes =
